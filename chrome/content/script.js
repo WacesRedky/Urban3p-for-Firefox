7076b1252ee5d72b0968951e2807b84e2f41adf8
WacesRedky <wacesredky@gmail.com>
2012-10-15 22:30:22 +0400

    with new tab opener, json and no-login

--- chrome/content/script.js
+++ chrome/content/script.js
@@ -1,137 +1,132 @@
-window.addEventListener ("load", urban3pLoad, false);
+window.addEventListener ("load", urban3pUpdateKarmaRepeatedly, false);
 
-function urban3pLoad() {
-	urban3pUpdateKarmaRepeatedly();
-}
+var username = '';
 
-function urban3pLeftClick(e)
-{
-	if (e.button == 0)
-	{
-		if (urban3pGetUsername())
-		{
+function urban3pLeftClick(e) {
+	if (e.button == 0) {
+		if (username) {
 			urban3pUpdateKarma();
 		} else {
-			urban3pShowPrefs();
+			urban3pLoadProfile(e);
 		}
 	}
 }
 
-function urban3pUpdateKarma()
-{
+function urban3pUpdateKarma() {
 	document.getElementById("urban3p-label").value = '…';
-	var username = urban3pGetUsername();
 
-	if (username)
-	{
-		var url = 'http://urban3p.ru/api/user/' + username.toLowerCase();
-		var httpRequest = new XMLHttpRequest();
-		if (httpRequest.overrideMimeType)
-		{
-			httpRequest.overrideMimeType('text/xml');
-		}
-
-		httpRequest.onreadystatechange = function() {
-			urban3pSetKarma(httpRequest);
-		};
-		httpRequest.open('GET', url, true);
-		httpRequest.send(null);
-   	}
-   	else
-	{
-		document.getElementById("urban3p-label").value = 'urban3p.ru';
-		document.getElementById("urban3p-label").tooltipText = 'Щелкните, чтобы настроить.';
+	var url = 'http://urban3p.ru/api/user/?format=json';
+	var xhr = new XMLHttpRequest();
+	if (xhr.overrideMimeType) {
+		xhr.overrideMimeType('text/xml');
 	}
+	xhr.onreadystatechange = urban3pSetKarma;
+	xhr.open('GET', url, true);
+	xhr.send(null);
 }
 
-function urban3pUpdateKarmaRepeatedly()
-{
+function urban3pUpdateKarmaRepeatedly() {
 	urban3pUpdateKarma();
-	setTimeout("urban3pUpdateKarmaRepeatedly()", 10*60*1000);
-
+	setTimeout(urban3pUpdateKarmaRepeatedly, 60*1000);
 }
 
-function urban3pSetKarma(httpRequest)
-{
-	if (httpRequest.readyState == 4)
-	{
+function urban3pSetKarma() {
+	if (this.readyState == 4) {
 		var label = ':-(';
-		var tooltip = 'Отсутствует связь с urban3p, либо задан неверный логин.';
-		try
-		{
-			if (httpRequest.status == 200)
-			{
-				var xmlDoc = httpRequest.responseXML;
-				var karma = xmlDoc.getElementsByTagName('karma').item(0).firstChild.data;
-				var rating = xmlDoc.getElementsByTagName('rating').item(0).firstChild.data;
-				var mailCount = xmlDoc.getElementsByTagName('mailCount').item(0).firstChild.data;
-				label = parseInt(karma).toString() + ' / ' + rating;
-				tooltip = "Карма / Рейтинг";
-				if (!urban3pIsMailCountHidden())
-				{
-					label += ' / ' + mailCount;
-					tooltip += " / Новая почта";
+			var tooltip = 'Отсутствует связь с urban3p.';
+			try {
+				if (this.status == 200) {
+					var response = JSON.parse(this.responseText);
+					username = response.login;
+					var karma = response.karma;
+					var rating = response.rating;
+					label = parseInt(karma, 10) + ' / ' + rating;
+					tooltip = "Карма / Рейтинг";
+					if (!urban3pIsMailCountHidden()) {
+						var mailCount = response.mailCount;
+						label += ' / ' + mailCount;
+						tooltip += " / Новая почта";
+					}
+				}
+			} catch (e) {
+			}
+
+			document.getElementById("urban3p-label").value = label;
+			document.getElementById("urban3p-label").tooltipText = tooltip;
+		}
+	}
+
+	function urban3pShowPrefs() {
+		window.openDialog("chrome://urban3p/content/prefs.xul", "_blank", "modal,centerscreen,chrome,resizable=no,dependent=yes");
+		urban3pUpdateKarma();
+	}
+	function urban3pIsMailCountHidden() {
+		return Components.classes["@mozilla.org/preferences-service;1"]
+		.getService(Components.interfaces.nsIPrefService)
+		.getBranch("extensions.urban3p.")
+		.getBoolPref("hideMailCount");
+	}
+	function urban3pLoadProfile(event) {
+		var url = 'http://urban3p.ru/my/';
+		urban3pLoadPage(event, url);
+	}
+	function urban3pUrlGo(event, url) {
+		urban3pLoadPage(event, 'http://urban3p.ru/'+url);
+	}
+	function urban3pLoadPage(event, url) {
+		var browser = document.getElementById("content");
+		if (event.button == 1) {
+			var tab = browser.addTab(url);
+			browser.selectedTab = tab;
+		} else if (event.button == 0) {
+			if (!event) {
+				openAndReuseOneTabPerURL(url);
+			}
+
+			if (event.shiftKey) {
+				openDialog("chrome://browser/content/browser.xul", "_blank", "chrome,all,dialog=no", url);
+			} else {
+				openAndReuseOneTabPerURL(url);
+			}
+		}
+	}
+
+	function openAndReuseOneTabPerURL(url) {
+		var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+		.getService(Components.interfaces.nsIWindowMediator);
+		var browserEnumerator = wm.getEnumerator("navigator:browser");
+
+		// Check each browser instance for our URL
+		var found = false;
+		while (!found && browserEnumerator.hasMoreElements()) {
+			var browserWin = browserEnumerator.getNext();
+			var tabbrowser = browserWin.gBrowser;
+
+			// Check each tab of this browser instance
+			var numTabs = tabbrowser.browsers.length;
+			for (var index = 0; index < numTabs; index++) {
+				var currentBrowser = tabbrowser.getBrowserAtIndex(index);
+				if (url == currentBrowser.currentURI.spec) {
+					// The URL is already opened. Select this tab.
+					tabbrowser.selectedTab = tabbrowser.tabContainer.childNodes[index];
+					// Focus *this* browser-window
+					browserWin.focus();
+
+					found = true;
+					break;
 				}
 			}
 		}
-		catch (e)
-		{
-		}
 
-		document.getElementById("urban3p-label").value = label;
-		document.getElementById("urban3p-label").tooltipText = tooltip;
-	}
-}
-
-function urban3pShowPrefs() {
-	window.openDialog("chrome://urban3p/content/prefs.xul", "_blank", "modal,centerscreen,chrome,resizable=no,dependent=yes");
-	urban3pUpdateKarma();
-}
-function urban3pGetUsername() {
-	return Components.classes["@mozilla.org/preferences-service;1"]
-	.getService(Components.interfaces.nsIPrefService)
-	.getBranch("extensions.urban3p.")
-	.getCharPref("username");
-}
-function urban3pIsMailCountHidden() {
-	return Components.classes["@mozilla.org/preferences-service;1"]
-	.getService(Components.interfaces.nsIPrefService)
-	.getBranch("extensions.urban3p.")
-	.getBoolPref("hideMailCount");
-}
-function urban3pLoadProfile(event) {
-	var username = urban3pGetUsername();
-	var url = 'http://urban3p.ru/user/'+username.toLowerCase()+'/';
-	urban3pLoadPage(event, url);
-}
-function urban3pUrlGo(event, url) {
-	urban3pLoadPage(event, 'http://urban3p.ru/'+url);
-}
-function urban3pLoadPage(event, url) {
-	var browser = document.getElementById("content");
-	if (event.button == 1)
-	{
-		var tab = browser.addTab(url);
-		browser.selectedTab = tab;
-	}
-	else if (event.button == 0)
-	{
-		if (!event)
-		{
-			browser.loadURI(url);
-			return;
-		}
-
-		var shift = event.shiftKey;
-		var ctrl =  event.ctrlKey;
-		if (shift)
-		{
-			openDialog("chrome://browser/content/browser.xul", "_blank", "chrome,all,dialog=no", url);
-		}
-		else
-		{
-			var tab = browser.addTab(url);
-			browser.selectedTab = tab;
+	// Our URL isn't open. Open it now.
+	if (!found) {
+		var recentWindow = wm.getMostRecentWindow("navigator:browser");
+		if (recentWindow) {
+			// Use an existing browser window
+			recentWindow.delayedOpenTab(url, null, null, null, null);
+		} else {
+			// No browser windows are open, so open a new one.
+			window.open(url);
 		}
 	}
 }
